0aacdf2a9142c2d1c37a921bdd2ea79315d58807
Marie-Jridi <74175840+Marie-Jridi@users.noreply.github.com>
2021-03-22 22:48:57 +0100

    Mise à jour du SolModifie.h

--- SolModifie.h
+++ SolModifie.h
@@ -2,50 +2,155 @@
 #include <math.h>
 #include <ode/common.h>
 
-const int Nombre_de_sommets = 11;
-const float R = 1.0f;
-
-float x1 = cos(54) * R;
-float y = sin(54) * R;
-float x2 = cos(18) * R;
-float y2 = sin(18) * R;
-float x3 = cos(18) * R;
-float y3 = -sin(18) * R;
-float x4 = cos(54) * R;
-float y4 = -sin(54) * R;
-float x5 = -cos(54) * R;
-float y5 = -sin(54) * R;
-float x6 = -cos(18) * R;
-float y6 = -sin(18) * R;
-float x7 = -cos(18) * R;
-float y7 = sin(18) * R;
-float x8 = -cos(54) * R;
-float y8 = sin(54) * R;
-
-
-float Aretes[Nombre_de_sommets * 3] = {
-	0.0f, 0.0f, 0.0f,
-	R, 0.0f, 0.0f,
-	x1, y, 0.0f,
-	x2, y2, 0.0f,
-	x3, y3, 0.0f,
-	x4, y4, 0.0f,
-	0.0f, R, 0.0f,
-	x5, y5, 0.0f,
-	x6, y6, 0.0f,
-	x7, y7, 0.0f,
-	x8, y8, 0.0f
+const int Nombre_de_sommets = 18;
+const int Nomdre_de_triangles = 48 * 3;
+const float HauteurMesh = 1.05f;
+const float R = 2.0f;
+float z = cos(54.0);
+float tabx[9] = {
+	0,
+	cos(45) * R,
+	cos(45) * R,
+	-cos(45) * R,
+	-cos(45) * R
+};
+float taby[9] = {
+	0,
+	sin(45) * R,
+	-sin(45) * R,
+	-sin(45) * R,
+	sin(45) * R
 };
 
-dTriIndex Triangles[Nombre_de_sommets - 1][3] = {
-	{0,1,2},
-	{0,2,3},
-	{0,3,4},
-	{0,4,5},
-	{0,5,6},
-	{0,6,7},
-	{0,7,8},
-	{0,8,9},
-	{0,9,10},
-	{0,10,1}
+float tabModif[8] = {
+	1.6,
+	0.8,
+	5.6,
+	2,
+	0.6,
+	3.1,
+	1.1,
+	0.2
+};
+
+float ArêtesGlace[Nombre_de_sommets * 3] = {
+	0.0f, 0.0f, HauteurMesh,
+	0.0f * tabModif[0], R * tabModif[0], HauteurMesh,
+	tabx[1] * tabModif[1], taby[1] * tabModif[1], HauteurMesh,
+	R * tabModif[2], 0.0f * tabModif[2], HauteurMesh,
+	tabx[2] * tabModif[3], taby[2] * tabModif[3], HauteurMesh,
+	0.0f * tabModif[4], -R * tabModif[4], HauteurMesh,
+	tabx[3] * tabModif[5], taby[3] * tabModif[5], HauteurMesh,
+	-R * tabModif[6], 0.0f * tabModif[6], HauteurMesh,
+	tabx[4] * tabModif[7], taby[4] * tabModif[7], HauteurMesh,
+
+	0.0f, 0.0f, -1.0f,
+	0.0f * tabModif[0], R * tabModif[0], -1.0f,
+	tabx[1] * tabModif[1], taby[1] * tabModif[1], -1.0f,
+	R * tabModif[2], 0.0f * tabModif[2], -1.0f,
+	tabx[2] * tabModif[3], taby[2] * tabModif[3], -1.0f,
+	0.0f * tabModif[4], -R * tabModif[4], -1.0f,
+	tabx[3] * tabModif[5], taby[3] * tabModif[5], -1.0f,
+	-R * tabModif[6], 0.0f * tabModif[6], -1.0f,
+	tabx[4] * tabModif[7], taby[4] * tabModif[7], -1.0f,
+};
+
+dTriIndex TrianglesGlace[Nomdre_de_triangles / 3][3] = {
+	//{0,1,2},
+	{2,1,0},
+
+	//{0,2,3},
+	{3,2,0},
+
+	//{0,3,4},
+	{4,3,0},
+
+	//{0,4,5},
+	{5,4,0},
+
+	//{0,5,6},
+	{6,5,0},
+
+	//{0,6,7},
+	{7,6,0},
+
+	//{0,7,8},
+	{8,7,0},
+
+	//{0,8,1},
+	{1,8,0},
+
+
+	//{9,10,11},
+	{11,10,9},
+
+	//{9,11,12},
+	{12,11,9},
+
+	//{9,12,13},
+	{13,12,9},
+
+	//{9,13,14},
+	{14,13,9},
+
+	//{9,14,15},
+	{15,14,9},
+
+	//{9,15,16},
+	{16,15,9},
+
+	//{9,16,17},
+	{17,16,9},
+
+	//{9,17,10},
+	{10,17,9},
+
+
+	{1,10,2},
+	{2,10,1},
+
+	{2,10,11},
+	{11,10,2},
+
+	{2,11,3},
+	{3,11,2},
+
+	{3,11,12},
+	{12,11,3},
+
+	{3,12,4},
+	{4,12,3},
+
+	{4,12,13},
+	{13,12,4},
+
+	{4,13,5},
+	{5,13,4},
+
+	{5,13,14},
+	{14,13,5},
+
+	{5,14,6},
+	{6,14,5},
+
+	{6,14,15},
+	{15,14,6},
+
+	{6,15,7},
+	{7,15,6},
+
+	{7,15,16},
+	{16,15,7},
+
+	{7,16,8},
+	{8,16,7},
+
+	{8,16,17},
+	{17,16,8},
+
+	{8,17,1},
+	{1,17,8},
+
+	{1,10,17},
+	{17,10,1}
 };
